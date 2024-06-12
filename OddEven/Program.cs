24acb7524db0b0834fd1df5d5b584763f278c691
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-12 10:38:06 +0300

    Initial commit

---
+++ OddEven/Program.cs
@@ -0,0 +1,12 @@
+﻿class OddEven {
+    public static void main (String[]args)
+    {
+
+        int number = int.Parse(Console.ReadLine());
+        if (number %2==0) {
+           Console.WriteLine("even");
+        } else {
+           Console.WriteLine("odd");
+        }
+    }
+}
